c0014507f14f3024662fc046b66c5f4946fb1b0a
kovacsadika <kovacs.adika@yahoo.com>
2020-05-05 17:58:56 +0100

    first day

---
+++ scripts.js
@@ -0,0 +1,17 @@
+$('#press').click(function (){
+    console.log("Yeah, you clicked me")
+})
+
+
+
+    
+    
+
+$('#change').click(function () {
+
+    for (let index = 0; index < 1; index++) {
+     var color = document.getElementById("color").value
+     $('button').css ('background', color)  } 
+    
+    
+})
